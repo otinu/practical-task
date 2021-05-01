441cf18a06b46f180d661f9f654607064cfbecb9
otinu <fitshter@gmail.com>
2021-05-02 08:20:07 +0900

    add_comments

--- bookers2-debug/bookers2-debug/config/routes.rb
+++ bookers2-debug/bookers2-debug/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:index,:edit,:update]
   patch 'users_path' => 'users#show'
   resources :books do
+    resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 end
