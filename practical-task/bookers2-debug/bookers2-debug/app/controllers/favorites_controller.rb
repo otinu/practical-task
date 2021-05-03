9734cf4f2ec49d51c97a897e5bf5a725e33853d6
otinu <fitshter@gmail.com>
2021-05-03 17:22:08 +0900

    save

--- practical-task/bookers2-debug/bookers2-debug/app/controllers/favorites_controller.rb
+++ practical-task/bookers2-debug/bookers2-debug/app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: params[:book_id])
+    @book = Book.find(params[:book_id]) #create.js.erbで利用するための準備
+    favorite = current_user.favorites.new(book_id: params[:book_id]) #current_userの持っているbook_idをfavoritesのbook_idへ
     favorite.save
     #redirect_back fallback_location: root_path
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite = current_user.favorites.find_by(book_id: params[:book_id])
     favorite.destroy
-    redirect_back fallback_location: root_path
+    #redirect_back fallback_location: root_path
   end
   
 end
