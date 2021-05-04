63aa7c013b4d2b65b8a8d4a8ebac671de0eefd7d
otinu <fitshter@gmail.com>
2021-05-04 15:14:20 +0900

    [Finish] practical-task7

--- practical-task/bookers2-debug/bookers2-debug/db/schema.rb
+++ practical-task/bookers2-debug/bookers2-debug/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_001823) do
+ActiveRecord::Schema.define(version: 2021_05_04_012041) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 2021_05_02_001823) do
     t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
