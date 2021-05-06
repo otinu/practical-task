076f72b9791a4d3c1d31e4debc1eaa0be81de3bb
otinu <fitshter@gmail.com>
2021-05-06 19:33:08 +0900

    make daily_mailer

--- bookers2-debug/app/mailers/daily_mailer.rb
+++ bookers2-debug/app/mailers/daily_mailer.rb
@@ -1,2 +1,6 @@
 class DailyMailer < ApplicationMailer
+    def daily_send(email)
+        default to: -> { User.pluck(:email) }
+        mail(subject: "Good morning! This is the Daily-Mail!")
+    end
 end
