59f9fed4e4b0838c60a34a9afa40f8b5c074374b
otinu <fitshter@gmail.com>
2021-05-10 08:24:53 +0900

    remake_mailer

--- bookers2-debug/app/mailers/thanks_mailer.rb
+++ bookers2-debug/app/mailers/thanks_mailer.rb
@@ -1,8 +1,15 @@
 class ThanksMailer < ApplicationMailer
 
-  
+=begin 
   def send_signup_email(email, name)
     @name = name
     mail to: email, subject: 'Registration Complete! Thanks for Joining!'
   end
+=end
+
+  def send_signup_email(user)
+    @name = user.name
+    mail(:subject => "登録完了のお知らせ", to: user.email)
+  end
+
 end
