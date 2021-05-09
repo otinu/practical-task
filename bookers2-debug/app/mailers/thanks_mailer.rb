6d7d4677c51274b6463dde7aecbf78389331341e
otinu <fitshter@gmail.com>
2021-05-10 08:27:29 +0900

    add comment

--- bookers2-debug/app/mailers/thanks_mailer.rb
+++ bookers2-debug/app/mailers/thanks_mailer.rb
@@ -8,8 +8,8 @@ class ThanksMailer < ApplicationMailer
 =end
 
   def send_signup_email(user)
-    @name = user.name
-    mail(:subject => "登録完了のお知らせ", to: user.email)
+    @name = user.name #模範解答はここでcurrent_userを全てインスタンス変数に格納していた。
+    mail(:subject => "登録完了のお知らせ", to: user.email) #current_userのパラメータは全てインスタンス変数に格納されているため、ここでNilのためにエラーが発生する。
   end
 
 end
